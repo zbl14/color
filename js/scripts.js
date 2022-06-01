d29ea4d84c11d1780882984481b3b889e6d5a5b0
zbl14 <ifthereisoneday@gmail.com>
2022-06-01 10:45:13 -0700

    add remote classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   $("button#green").click(function() {
+    $("body").removeClass();
     $("body").addClass("green-background");
   });
 
   $("button#yellow").click(function() {
+    $("body").removeClass();
     $("body").addClass("yellow-background");
   });
 
   $("button#red").click(function() {
+    $("body").removeClass();
     $("body").addClass("red-background");
   });
+
 });
